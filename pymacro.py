3cb24ddbc69c97ebff5e0b4f1c753fd99b382865
Ilia Lebedev <ilebedev@mit.edu>
2017-09-08 01:18:58 -0400

    bugfix

--- pymacro.py
+++ pymacro.py
@@ -4,12 +4,12 @@ import sys, re, itertools
 
 def print_usage():
   print "PyMacro is a preprocessor for powerful verilog metaprogramming. Usage:"
-  print "%s <file.pm> [token='$']" % sys.argv[0]
-  print "The *.pv file is standard verilog with embedded segments of arbitrary python: '$$EXPRESSION$$', or '$!ARBITRARY_CODE!$'."
-  print "If a custom token is defined, say \%\%\%, the above becomes \%\%\%$EXPRESSION$\%\%\%', or '\%\%\%!ARBITRARY_CODE!\%\%\%"
-  print "Any printouts from code inside $!...!$, and the result of evaluating expressions inside $$...$$ is inserted in place of these segments."
-  print "Pro tip: You can use #import statements"
-  print "There is no library; anything you need you write yourself."
+  print "%s <file> [token='$']" % sys.argv[0]
+  print "The macro processor requires a unique token to identify python segments, one that should not be used in the file elsewhere. By default, the unique token is \"$\"."
+  print "The file is text with embedded segments of arbitrary python: '$(EXPRESSION)$', or '${ARBITRARY_CODE}$'."
+  print "If a custom token is defined, say %%%, the above becomes %%%(EXPRESSION)%%%', or '%%%{ARBITRARY_CODE}%%%"
+  print "Any printouts from python code, and the result of evaluating expressions is inserted in place of these segments."
+  print "There is no standard library; anything you need you write yourself. Pro tip: You can use #import statements to reuse library code."
 
 file_scope = {}
 
@@ -29,16 +29,15 @@ def main(filename = None, args={}):
   # Chop file up into verilog and python tokens.
   # Odd elements are python, of the form (expr?, py_str), where expr? is a boolean denoting that the py_str is an EXPRESSION, and not a block of code.
   file = re.sub(r"\r?\n", ("%s#" % token), file) # Hide newlines for now
-  segments = list(filter(None, re.split(r"%s([\$\!].*?)[\$\!]%s" % (re.escape(token), re.escape(token)), file, re.MULTILINE)))
-  v_segments = [re.sub("%s#" % re.escape(token), "\n", s) for s in segments[0::2]]
-  py_segments = [((s[0] == "$"),  re.sub(r"%s#" % re.escape(token), "\n", s[1:])) for s in segments[1::2]]
+  segments = re.split(r"(%s[\(\{].*?)[\)\}]%s" % (re.escape(token), re.escape(token)), file, flags=re.MULTILINE)
+  v_segments = [re.sub("%s#"%re.escape(token), "\n", s) for s in segments[0::2]]
+  py_segments = [(s[0:1+len(token)]==("%s("%token), re.sub(r"%s#"%re.escape(token), "\n", s[(1+len(token)):])) for s in segments[1::2] ]
 
   #evaluate/execute py_segements in a presistent scope
-  def do_exec(str):
-    exec(str) in file_scope
+  def do_exec(statements):
+    exec(statements) in file_scope
     return ""
-  print py_segments
-  py_results = [( eval(s[1], file_scope) if s[0] else do_exec(s[1]) ) for s in py_segments]
+  py_results = [( eval(s[1].strip(), file_scope) if s[0] else do_exec(s[1].strip()) ) for s in py_segments]
 
   # Join result into a string
   result = "".join(list(str(it.next()) for it in itertools.cycle([iter(v_segments), iter(py_results)])))
